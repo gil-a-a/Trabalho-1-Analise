d3b6777b7c398523951bcda597bf57994c7d1283
pedro-bruce <98055885+pedro-bruce@users.noreply.github.com>
2022-10-19 00:34:28 -0300

    Update Trabalho 1.c

--- Trabalho 1.c
+++ Trabalho 1.c
@@ -14,22 +14,22 @@ int* geraVetor(int tam);
 void printaVetor(int *vet, int tam);
 //============== Sorts ==============//
 //Insertion
-void insertionSort(int *vet, int tam);
+void insertionSort(int *vet, int tam, struct dados_da_amostra *dado);
 
 //Selection
 void selectionSort(int *vet, int tam, struct dados_da_amostra *dado);
 
 //Merge
-void mergeSort(int *vet, int comeco, int fim);
-void intercala(int *vet, int comeco, int meio, int fim);
+void mergeSort(int *vet, int comeco, int fim, struct dados_da_amostra *dados);
+void intercala(int *vet, int comeco, int meio, int fim, struct dados_da_amostra *dados);
 
 //Heap
-void maxHeapify(int *vet, int tam, int i);
-void heapSort(int *vet, int tam);
+void maxHeapify(int *vet, int tam, int i, struct dados_da_amostra *dados);
+void heapSort(int *vet, int tam, struct dados_da_amostra *dados);
 
 //Quick
-int partition(int *vet, int p, int r);
-void quickSort(int *vet, int p, int r);
+int partition(int *vet, int p, int r, struct dados_da_amostra *dados);
+void quickSort(int *vet, int p, int r, struct dados_da_amostra *dados);
 //===================================//
 double calculaMedia(int *valores, int tam)
 {
@@ -45,8 +45,28 @@ int main ()
 {
 	int tam;
 	int *vet;
-	struct dados_da_amostra dados[3];
+	//struct dados_da_amostra dados[3];
+    //struct dados_da_amostra dados_merge;
+    //struct dados_da_amostra dados_heap;
+    struct dados_da_amostra dados_quick;
+
+    /*dados_merge.num_comparacoes = 0;
+    dados_merge.num_trocas = 0;
+    dados_merge.tempo_de_execucao = 0;
+*/
+   /* dados_heap.num_comparacoes = 0;
+    dados_heap.num_trocas = 0;
+    dados_heap.tempo_de_execucao = 0;
+*/
+    dados_quick.num_comparacoes = 0;
+    dados_quick.num_trocas = 0;
+    dados_quick.tempo_de_execucao = 0;
 	
+
+    //clock_t tempo1_merge, tempo2_merge;
+    //clock_t tempo1_heap, tempo2_heap;
+    clock_t tempo1_quick, tempo2_quick;
+
 	srand(time(NULL));
 	
 	printf("Insira o tamanho do vetor: ");
@@ -55,21 +75,35 @@ int main ()
 	
 	vet = geraVetor(tam);
 	printf("\nVetor gerado: ");
-	printaVetor(vet, tam);
+	//printaVetor(vet, tam);
 	
 	printf("\nVetor organizado: ");
 //	insertionSort(vet, tam);
-	selectionSort(vet, tam, &dados[0]);
-//	mergeSort(vet, 0, tam - 1);
-//	heapSort(vet, tam);
-//	quickSort(vet, 0, tam-1);
+//	selectionSort(vet, tam, &dados[0]);
+
+/*    tempo1_merge = clock();
+	mergeSort(vet, 0, tam - 1, &dados_merge);
+    tempo2_merge = clock();
+    dados_merge.tempo_de_execucao = (double)(tempo2_merge - tempo1_merge)/CLOCKS_PER_SEC;
+    printf("tempo: %f\nnum comp: %d\ntrocas: %d\n", dados_merge.tempo_de_execucao, dados_merge.num_comparacoes, dados_merge.num_trocas);
+*/	
+    /*tempo1_heap = clock();
+    heapSort(vet, tam, &dados_heap);
+    tempo2_heap = clock();
+    dados_heap.tempo_de_execucao = (double)(tempo2_heap - tempo1_heap)/CLOCKS_PER_SEC;
+    printf("tempo: %f\nnum comp: %d\ntrocas: %d\n", dados_heap.tempo_de_execucao, dados_heap.num_comparacoes, dados_heap.num_trocas);
+*/	
+    tempo1_quick = clock();
+    quickSort(vet, 0, tam-1, &dados_quick);
+	tempo2_quick = clock();
+    dados_quick.tempo_de_execucao = (double)(tempo2_quick - tempo1_quick)/CLOCKS_PER_SEC;
+	printf("tempo: %f\nnum comp: %d\ntrocas: %d\n", dados_quick.tempo_de_execucao, dados_quick.num_comparacoes, dados_quick.num_trocas);
+    printaVetor(vet, tam);
 	
-	printaVetor(vet, tam);
-	
-	printf("Tempo de exec: %f\n", dados[0].tempo_de_execucao);
+	/*printf("Tempo de exec: %f\n", dados[0].tempo_de_execucao);
 	printf("Num comparacoes: %d\n", dados[0].num_comparacoes);
 	printf("Num trocas: %d\n", dados[0].num_trocas);
-	
+	*/
 	return 0;
 }
 
@@ -93,20 +127,34 @@ void printaVetor(int *vet, int tam)
 
 //============== Sorts ==============//
 //Insertion
-void insertionSort(int *vet, int tam)
+void insertionSort(int *vet, int tam, struct dados_da_amostra *dado)
 {
 	int i, j, chave;
-	
+	clock_t tempo1, tempo2;
+
+	dado->tempo_de_execucao = 0;
+	dado->num_trocas = 0;
+	dado->num_comparacoes = 0;
+
+	tempo1 = clock();
+
 	for (i = 1; i < tam; i++){
 		chave = vet[i];
 		j = i - 1;
 		while (j >= 0 && vet[j] > chave){
 			vet[j+1] = vet[j];
 			j = j-1;
+            dado->num_trocas++;
+            dado->num_comparacoes++;
 		}
-		
+
+		dado->num_comparacoes++;
 		vet[j + 1] = chave;
 	}
+
+	tempo2 = clock();
+
+	dado->tempo_de_execucao = (double) (tempo2 - tempo1)/CLOCKS_PER_SEC;	
 }
 
 //Selection
@@ -142,116 +190,135 @@ void selectionSort(int *vet, int tam, struct dados_da_amostra *dado)
 }
 
 //Merge
-void intercala(int *vet, int comeco, int meio, int fim) {
+void intercala(int *vet, int comeco, int meio, int fim, struct dados_da_amostra *dados) {
 	int temp[fim - comeco + 1];
 	int pos = 0, pos_comeco = comeco, pos_final = meio + 1;
-   
+
 	while (pos_comeco <= meio && pos_final <= fim){
     	if (vet[pos_comeco] < vet[pos_final]){
         	temp[pos++] = vet[pos_comeco++];
+            dados->num_trocas++;
     	}
     	else{
         	temp[pos++] = vet[pos_final++];
+            dados->num_trocas++;
     	}
+
+        dados->num_comparacoes++;
 	}
 
 	while (pos_comeco <= meio){
 		temp[pos++] = vet[pos_comeco++];
+        dados->num_trocas++;
 	}
 
 	while (pos_final <= fim){
 		temp[pos++] = vet[pos_final++];
+        dados->num_trocas++;
 	}
 
-   int i;
-   for (i = 0; i < pos; i++){
-      vet[i + comeco] = temp[i];
-   }
-   return;
+    int i;
+    for (i = 0; i < pos; i++){
+        vet[i + comeco] = temp[i];
+    }
+
+    return;
 }
 
-void mergeSort(int *vet, int comeco, int fim){
+void mergeSort(int *vet, int comeco, int fim, struct dados_da_amostra *dados){
     int meio = (fim+comeco)/2;
 	if (comeco < fim) {
-        mergeSort(vet, comeco, meio);
-        mergeSort(vet, meio+1, fim);
-        intercala(vet, comeco, meio, fim);
+        mergeSort(vet, comeco, meio, dados);
+        mergeSort(vet, meio+1, fim, dados);
+        intercala(vet, comeco, meio, fim, dados);
     }
+
+    dados->num_comparacoes++;
 }
 
 //Heap
-void maxHeapify(int *vet, int tam, int i)
+void maxHeapify(int *vet, int tam, int i, struct dados_da_amostra *dados)
 {
 	int aux;
     int maior = i;
     int e = 2*i + 1;
     int d = 2*i + 2;
- 
+    
+    dados->num_comparacoes++;
     if (e < tam && vet[e] > vet[maior]){
     	maior = e;
 	}
- 
+    
+    dados->num_comparacoes++;
     if (d < tam && vet[d] > vet[maior]){
     	maior = d;
 	}
- 
+    
+    dados->num_comparacoes++;
     if (maior != i) {
     	aux = vet[i];
     	vet[i] = vet[maior];
     	vet[maior] = aux;
     	
-        maxHeapify(vet, tam, maior);
+        dados->num_trocas++;
+        maxHeapify(vet, tam, maior, dados);
     }
 }
 
-void heapSort(int *vet, int tam)
+void heapSort(int *vet, int tam, struct dados_da_amostra *dados)
 {
 	int i, aux;
 	
     for (i = tam/2 - 1; i >= 0; i--) {
-    	maxHeapify(vet, tam, i);
+    	maxHeapify(vet, tam, i, dados);
 	}
 	
 	for (i = tam - 1; i >= 0; i--) {
     	aux = vet[0];
     	vet[0] = vet[i];
     	vet[i] = aux;
- 
-        maxHeapify(vet, i, 0);
+
+        dados->num_trocas++;
+        maxHeapify(vet, i, 0, dados);
 	}
 }
 
 //Quick
-int partition(int *vet, int p, int r)
+int partition(int *vet, int p, int r, struct dados_da_amostra *dados)
 {
 	int aux;
 	int x = vet[r]; // pivô
 	int i = p-1, j;
 
 	for (j = p; j <= r-1; j++){
+        
+        dados->num_comparacoes++;
 		if (vet[j] < x) {
 			i++;
 			aux = vet[i];
 			vet[i] = vet[j];
 			vet[j] = aux;
+            dados->num_trocas++;
 		}
 	}
 	
 	aux = vet[i + 1];
 	vet[i + 1] = vet[r];
 	vet[r] = aux;
-	
+	dados->num_trocas++;
+
 	return i + 1;
 }
 
-void quickSort(int *vet, int p, int r)
+void quickSort(int *vet, int p, int r, struct dados_da_amostra *dados)
 {
 	int q;
 	
+
 	if (p < r) {
-		q = partition(vet, p, r);	
-		quickSort(vet, p, q-1);
-		quickSort(vet, q+1, r);
+		q = partition(vet, p, r, dados);	
+		quickSort(vet, p, q-1, dados);
+		quickSort(vet, q+1, r, dados);
 	}
 }
 //===================================//
